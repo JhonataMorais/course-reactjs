856b8a1d70f9a84a884cd87d6c485d3d4543201b
Jhow <jhonata.miranda@tatix.com.br>
2019-04-03 19:51:21 -0300

    Buscando produtos da API

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -1,8 +1,60 @@
 import React, { Component } from 'react';
 import api from '../../services/api';
+import styles from './styles.css';
 
 export default class Main extends Component {
+	state = {
+		products: [],
+		productInfo: {},
+	}
+
+	componentDidMount() {
+		this.loadProducts();
+	}
+
+	loadProducts = async (page = 1) => {
+		const res = await api.get(`/products?page=${page}`);
+		
+		const { docs, ...productInfo } = res.data;
+
+		this.setState({
+			products: res.data.docs,
+			productInfo
+		})
+	}
+
+	prevPage = () => {
+
+	}
+
+	nextPage = () => {
+		const { page, productInfo } = this.state;
+
+		if (page === productInfo.pages) return;
+
+		const pageNumber = page + 1;
+
+		this.loadProducts(pageNumber)
+	}
+
     render() {
-        return <h1> Grau é arte </h1>
+        return (
+        	<div className="product-list">
+        		{
+        			this.state.products.map(product => (
+        				<article key={product._id} >
+        					<strong>{product.title}</strong>
+        					<p>{product.description}</p>
+
+        					<a href="">Acessar</a>
+        				</article>
+        			))
+        		}
+        		<div className="actions">
+        			<button onClick={this.prevPage}>Anterior</button>
+        			<button onClick={this.nextPage}>Próximo</button>
+        		</div>
+        	</div>
+        )
     }
 }
